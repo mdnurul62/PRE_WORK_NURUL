a8a506f2217ed117520765feaa5ad1332b680614
mdnurul62 <nurul.alam1@hotmail.com>
2017-04-01 21:40:34 -0400

    Prework modules added

---
+++ Module-10/myScript.js
@@ -0,0 +1,22 @@
+
+		document.getElementById("button1").addEventListener("click", function(){
+				document.getElementById("box").style.height = "250px";
+			});
+        
+        document.getElementById("button2").addEventListener("click", function(){
+        		document.getElementById("box").style.backgroundColor = "blue";
+        	});         
+       
+        document.getElementById("button3").addEventListener("click", function(){
+            document.getElementById("box").style.backgroundColor = "cyan";
+      		});
+        
+        document.getElementById("button4").addEventListener("click", function(){
+            document.getElementById("box").style.height = "150px";
+       		});
+         document.getElementById("button4").addEventListener("click", function(){
+            document.getElementById("box").style.backgroundColor = "orange";
+       		});
+         document.getElementById("button5").addEventListener("click", function(){
+            document.getElementById("box").style.fontSize = "20px";
+       		});
